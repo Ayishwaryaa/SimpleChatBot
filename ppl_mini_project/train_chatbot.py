2c161f28d3084c6564cf819ec15e51fa99d10c1f
Ayishwaryaa <112137001+Ayishwaryaa@users.noreply.github.com>
2024-05-14 19:06:05 +0530

    train_chatbot.py

--- ppl_mini_project/train_chatbot.py
+++ ppl_mini_project/train_chatbot.py
@@ -34,35 +34,34 @@ for intent in intents['intents']:
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
-# lemmaztize and lower each word and remove duplicates
+# lemmaztizing and lowering each word and removing duplicates from each word
 words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
 words = sorted(list(set(words)))
-# sort classes
+# sorting classes
 classes = sorted(list(set(classes)))
-# documents = combination between patterns and intents
+# documents is a combination between patterns and intents
 print (len(documents), "documents")
-# classes = intents
+# classes => is nothing but intents
 print (len(classes), "classes", classes)
-# words = all words, vocabulary
+# words => all words,( vocabulary )
 print (len(words), "unique lemmatized words", words)
 
 
 pickle.dump(words,open('words.pkl','wb'))
 pickle.dump(classes,open('classes.pkl','wb'))
 
-# create our training data
+# creating the training data
 training = []
-# create an empty array for our output
+# creating an empty array for the output
 output_empty = [0] * len(classes)
 # training set, bag of words for each sentence
 for doc in documents:
-    # initialize our bag of words
-    bag = []
-    # list of tokenized words for the pattern
+    # initializing the bag of words
+    bag = []   # list of tokenized words for the pattern
     pattern_words = doc[0]
-    # lemmatize each word - create base word, in attempt to represent related words
+    # lemmatizing each word - creating base word to represent related words
     pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
-    # create our bag of words array with 1, if word match found in current pattern
+    # creating the bag of words array with 1, if word match found in current pattern
     for w in words:
         bag.append(1) if w in pattern_words else bag.append(0)
     
@@ -71,19 +70,19 @@ for doc in documents:
     output_row[classes.index(doc[1])] = 1
     
     training.append([bag, output_row])
-# shuffle our features and turn into np.array
+# shuffling the features and turning it into array
 random.shuffle(training)
 training = np.array(training, dtype=object)
 
-# create train and test lists. X - patterns, Y - intents
+# creating train and test lists. X => patterns, Y => intents
 train_x = list(training[:,0])
 train_y = list(training[:,1])
 print("Training data created")
 
-
-# Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
+# Now its time to create the model...
+# Creating model with 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
 # equal to number of intents to predict output intent with softmax
-model = Sequential()
+model = Sequential()   #creating sequential model
 model.add(Input(shape=(len(train_x[0]),)))
 model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
@@ -91,7 +90,7 @@ model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
 
-# Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
+# Compiling model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
 #sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 sgd = SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
 
