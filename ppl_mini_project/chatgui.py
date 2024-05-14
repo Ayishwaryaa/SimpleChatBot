f736fee574ae85538fd992ad443e405bf9fd5dc6
Ayishwaryaa <112137001+Ayishwaryaa@users.noreply.github.com>
2024-05-14 19:10:28 +0530

    chatgui.py

--- ppl_mini_project/chatgui.py
+++ ppl_mini_project/chatgui.py
@@ -14,35 +14,35 @@ classes = pickle.load(open('classes.pkl','rb'))
 
 
 def clean_up_sentence(sentence):
-    # tokenize the pattern - split words into array
+    # tokenizing the pattern => splitting words into array
     sentence_words = nltk.word_tokenize(sentence)
-    # stem each word - create short form for word
+    # stemming each word => creating short form for word
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
 
-# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+# returning bag of words array: 0 or 1 for each word in the bag that exists in the sentence
 
 def bow(sentence, words, show_details=True):
-    # tokenize the pattern
+    # tokenizing the pattern
     sentence_words = clean_up_sentence(sentence)
-    # bag of words - matrix of N words, vocabulary matrix
+    # bag of words => matrix of N words, vocabulary matrix
     bag = [0]*len(words)  
     for s in sentence_words:
         for i,w in enumerate(words):
             if w == s: 
-                # assign 1 if current word is in the vocabulary position
+                # assigning 1 if current word is in the vocabulary position
                 bag[i] = 1
                 if show_details:
                     print ("found in bag: %s" % w)
     return(np.array(bag))
 
 def predict_class(sentence, model):
-    # filter out predictions below a threshold
+    # filtering out predictions below a threshold
     p = bow(sentence, words,show_details=False)
     res = model.predict(np.array([p]))[0]
     ERROR_THRESHOLD = 0.25
     results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
-    # sort by strength of probability
+    # sorting by strength of probability
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
     for r in results:
@@ -64,7 +64,7 @@ def chatbot_response(msg):
     return res
 
 
-#Creating GUI with tkinter
+#Creating GUI using tkinter library
 import tkinter
 from tkinter import *
 
@@ -90,26 +90,25 @@ base.title("ChatWithMe")
 base.geometry("400x460")
 base.resizable(width=FALSE, height=FALSE)
 
-#Create Chat window
+#Creating Chat window
 ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Arial",)
 
 ChatLog.config(state=DISABLED)
 
-#Bind scrollbar to Chat window
+#Binding scrollbar to Chat window
 scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="arrow")
 ChatLog['yscrollcommand'] = scrollbar.set
 
-#Create Button to send message
+#Creating Button to send message
 SendButton = Button(base, font=("Verdana",12,'bold'), text="Send", width=10, height=2,
                     bd=0, bg="#df96fa", activebackground="#d362fc",fg='#3d0823',
                     command= send )
 
- #Create the box to enter message
+ #Creating the box to enter message
 EntryBox = Text(base, bd=1, bg="white",width="29", height="5", font="Arial")
-#EntryBox.bind("<Return>", send)
 
 
-#Place all components on the screen
+#Placing all the components on to the screen
 scrollbar.place(x=376,y=6, height=386)
 ChatLog.place(x=6,y=6, height=386, width=370)
 SendButton.place(x=270, y=401, height=50)
